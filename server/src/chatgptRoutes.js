ce2aac64472f86e3a8ddd194b484b368fc7d86fc
Rupali Haldiya <rupali7487@gmail.com>
2023-07-17 14:57:33 +0530

    remove max_tokens

--- server/src/chatgptRoutes.js
+++ server/src/chatgptRoutes.js
@@ -6,7 +6,6 @@ const makeRequest = async (prompt) => {
     "https://api.openai.com/v1/chat/completions",
     {
       model: "gpt-3.5-turbo",
-      max_tokens: 250,
       messages: [{ role: "user", content: prompt + 'please make sure the res is complete' }],
     },
     {
@@ -24,21 +23,23 @@ router.post("/send-prompt", async (req, res) => {
     let finishedReason = ""
     let prompt = req.body.prompt
     let maxLimit = 5
-    do {
-      const response = await makeRequest(prompt);
-      finishedReason = response.finish_reason
+    // do {
+    //   const response = await makeRequest(prompt);
+    //   finishedReason = response.finish_reason
 
-      completeResponse += response.message.content
-      prompt = `please complete this response: ${completeResponse}`
+    //   completeResponse += response.message.content
+    //   prompt = `please complete this response: ${completeResponse}`
 
-      if(maxLimit == 0 || finishedReason == 'stop') {
-        break;           
-      }
-      maxLimit -= 1
+    //   if(maxLimit == 0 || finishedReason == 'stop') {
+    //     break;           
+    //   }
+    //   maxLimit -= 1
      
-    } while(maxLimit > 0);
+    // } while(maxLimit > 0);
 
-    res.send({status: 'success', data: completeResponse});
+    const response = await makeRequest(prompt);
+
+    res.send({status: 'success', data: response.message.content});
   } catch (err) { 
     res.send({ status: "error", message: err.message });
   }
