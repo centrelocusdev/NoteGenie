a4a43e4ef6427ffeef1c58aebdb2fdf1a056678e
Centrelocus <127776664+centrelocusdev@users.noreply.github.com>
2024-01-31 16:52:26 +0530

    Delete server directory

--- server/predefinedTemplateModal.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-const templateModal = mongoose.Schema({
-  name: String,
-  description: String,
-  profession: String
-})
-
-module.exports = mongoose.model('predefined_templates', templateModal)
